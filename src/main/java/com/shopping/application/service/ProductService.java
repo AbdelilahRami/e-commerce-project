3040e7bbbdaea93026968c53f676df9b54b2798a
RAMI <ramiabdelilah1@gmail.com>
2021-02-17 16:57:55 +0100

    product-service

--- src/main/java/com/shopping/application/service/ProductService.java
+++ src/main/java/com/shopping/application/service/ProductService.java
@@ -1,8 +1,17 @@
 package com.shopping.application.service;
 
 
+import com.shopping.application.repositorie.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
 }
