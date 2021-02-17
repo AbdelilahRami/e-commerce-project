a1c6f3791db83ce393dc3fe187fbecf69fdd1bf8
RAMI <ramiabdelilah1@gmail.com>
2021-02-17 15:52:34 +0100

    add ProductDto

---
+++ src/main/java/com/shopping/application/dto/ProductDto.java
@@ -0,0 +1,20 @@
+package com.shopping.application.dto;
+
+import lombok.*;
+
+@Data
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    private String id;
+    private String productName;
+    private String description;
+    private String brandName;
+    private double price;
+    private int quantity;
+    private String user;
+    private String productCategory;
+
+}
