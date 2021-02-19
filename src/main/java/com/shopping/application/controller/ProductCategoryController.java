4ceb2ae8d99330f967c9701afdfeacc36e6609ec
RAMI <ramiabdelilah1@gmail.com>
2021-02-19 16:12:03 +0100

    product-category-controller

--- src/main/java/com/shopping/application/controller/ProductCategoryController.java
+++ src/main/java/com/shopping/application/controller/ProductCategoryController.java
@@ -1,27 +1,39 @@
 package com.shopping.application.controller;
 
 import com.shopping.application.dto.ProductCategoryDto;
+import com.shopping.application.mapper.ProductCategoryMapper;
+import com.shopping.application.models.ProductCategory;
 import com.shopping.application.service.ProductCategoryService;
 import com.shopping.application.service.ProductService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/categories")
 public class ProductCategoryController {
 
     private final ProductCategoryService productCategoryService;
+    private final ProductCategoryMapper productCategoryMapper;
 
-    public ProductCategoryController(ProductCategoryService productCategoryService) {
+    public ProductCategoryController(ProductCategoryService productCategoryService, ProductCategoryMapper productCategoryMapper) {
         this.productCategoryService = productCategoryService;
+        this.productCategoryMapper = productCategoryMapper;
     }
 
     @PostMapping
-    public void createCategory(@RequestBody ProductCategoryDto productCategoryDto){
+    public void createCategory(@RequestBody @Validated ProductCategoryDto productCategoryDto){
 
         productCategoryService.create(productCategoryDto);
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductCategoryDto> getProductCategory(@PathVariable String id ){
+        Long productCategoryId = Long.parseLong(id);
+        ProductCategoryDto productCategoryDto = productCategoryService.getProductCategory(productCategoryId)
+                                            .map(productCategoryMapper::productCategoryToProductCategoryDto)
+                                            .orElseThrow(NullPointerException::new);
+        return ResponseEntity.ok().body(productCategoryDto);
     }
 }
