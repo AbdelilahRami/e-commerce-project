c342437183b830cb687b242f366593419ab532dd
RAMI <ramiabdelilah1@gmail.com>
2021-02-23 14:44:35 +0100

    product-mapper-refactoring

--- src/main/java/com/shopping/application/mapper/ProductMapper.java
+++ src/main/java/com/shopping/application/mapper/ProductMapper.java
@@ -6,12 +6,15 @@ import com.shopping.application.models.ProductCategory;
 import com.shopping.application.models.User;
 import com.shopping.application.repositorie.ProductCategoryRepository;
 import com.shopping.application.repositorie.ProductRepository;
+import com.shopping.application.repositorie.UserRepository;
 import lombok.NoArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -23,44 +26,47 @@ public abstract class ProductMapper {
     private  ProductRepository productRepository;
     @Autowired
     private  ProductCategoryRepository productCategoryRepository;
+    @Autowired
+    private UserRepository userRepository;
 
 
     @Mapping(target ="id", expression = "java(mapUUIDToString(product))")
-    @Mapping(target ="user", expression = "java(getUserName(product))")
+    @Mapping(target ="user", expression = "java(getUserId(product))")
     @Mapping(target ="productCategory", expression = "java(getCategoryName(product))")
     public abstract ProductDto productToProductDTO(Product product);
-
-    @Mapping(target ="id", expression = "java(mapStringToUUID(productDto.getId()))")
-    @Mapping(target ="userId", expression = "java(getUserByUsername(productDto))")
-    @Mapping(target ="productCategory", expression = "java(getProductCategory(productDto))")
-    public abstract Product productDTOToProduct(ProductDto productDto);
 
     public String mapUUIDToString(Product product){
         UUID uuid = product.getId();
         return uuid != null ? uuid.toString() : null;
     }
+    String getUserId(Product product){
+        User user = product.getUser();
+        return (user != null) ? user.getId().toString() : null;
+    }
+    String getCategoryName(Product product){
+        return product.getProductCategory().getCategoryName();
+    }
+
+
+    @Mapping(target ="id", expression = "java(mapStringToUUID(productDto.getId()))")
+    @Mapping(target ="user", expression = "java(getUserByProductDto(productDto))")
+    @Mapping(target ="productCategory", expression = "java(getProductCategory(productDto))")
+    public abstract Product productDTOToProduct(ProductDto productDto);
 
     public UUID mapStringToUUID(String id){
         return id!=null ? UUID.fromString(id): null;
     }
 
-     String getUserName(Product product){
-        User user = product.getUserId();
-        return user.getFirstName();
-    }
-     User getUserByUsername(ProductDto productDto){
-         String userName= productDto.getUser();
-         return User.builder().firstName("username").build();
+    ProductCategory getProductCategory(ProductDto productDto){
+        String categoryName = productDto.getProductCategory();
+        ProductCategory productCategory = productCategoryRepository.getProductCategoryByCategoryName(categoryName).orElse(null);
+        return productCategory;
     }
 
-     String getCategoryName(Product product){
-        return product.getProductCategory().getCategoryName();
-    }
-
-     ProductCategory getProductCategory(ProductDto productDto){
-         String categoryName = productDto.getProductCategory();
-         ProductCategory productCategory = productCategoryRepository.getProductCategoryByCategoryName(categoryName).orElse(null);
-         return productCategory;
+     User getUserByProductDto(ProductDto productDto){
+         String userId= productDto.getUser();
+         User user = userId != null ? userRepository.findById(UUID.fromString(userId)).orElse(null): null;
+         return user;
     }
 
     public List<ProductDto> mapProductsDToToProducts(List<Product> products){
