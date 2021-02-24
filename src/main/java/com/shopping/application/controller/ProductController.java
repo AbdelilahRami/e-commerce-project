1ff0246d11248bb78fb74387b13687745108c149
RAMI <ramiabdelilah1@gmail.com>
2021-02-24 17:26:37 +0100

    product-controller-product-service

--- src/main/java/com/shopping/application/controller/ProductController.java
+++ src/main/java/com/shopping/application/controller/ProductController.java
@@ -1,25 +1,34 @@
 package com.shopping.application.controller;
 
 import com.shopping.application.dto.ProductDto;
+import com.shopping.application.exception.NoCategoryException;
+import com.shopping.application.exception.ProductNotFoundException;
+import com.shopping.application.mapper.ProductMapper;
+import com.shopping.application.models.Product;
 import com.shopping.application.service.ProductCategoryService;
 import com.shopping.application.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
+    private final String NO_CATEGORY_FOUND_404 = "N0_CATEGORY_FOUND_404";
+
     private final ProductService productService;
     private final ProductCategoryService productCategoryService;
+    private final ProductMapper productMapper;
 
-
-    public ProductController(ProductService productService, ProductCategoryService productCategoryService) {
+    public ProductController(ProductService productService, ProductCategoryService productCategoryService, ProductMapper productMapper) {
         this.productService = productService;
         this.productCategoryService = productCategoryService;
+        this.productMapper = productMapper;
     }
 
     @GetMapping
@@ -28,20 +37,38 @@ public class ProductController {
         return productService.getAll();
     }
     @GetMapping("/{id}")
-    private ProductDto getProduct(@PathVariable String id){
-
-        return productService.getProductById(id);
+    private ResponseEntity<ProductDto> getProduct(@PathVariable String id){
+        Product product = productService.getProductById(id).orElseThrow(ProductNotFoundException::new);
+        return new ResponseEntity<>(productMapper.productToProductDTO(product), HttpStatus.OK);
 
     }
 
     @PostMapping
-    private ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto){
-       if(!productDto.hasCategoryDto() | !productCategoryService.isExistedCategory(productDto.getProductCategory())){
-           throw new NullPointerException("N0_CATEGORY_FOUND_404");
-       }
-       return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(productDto));
+    private ResponseEntity<ProductDto> createProduct(@RequestBody @Valid ProductDto productDto){
+        checkProductCategoryExistence(productDto, NO_CATEGORY_FOUND_404);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(productDto));
 
     }
 
+    @PutMapping("/{id}")
+    private  ResponseEntity<ProductDto> updateProduct(@PathVariable String id,@RequestBody ProductDto productDto){
+        checkProductCategoryExistence(productDto,  NO_CATEGORY_FOUND_404);
+        ProductDto updatedProductDTO = productService.updateProduct(productDto, id).map(productMapper::productToProductDTO).orElse(null);
+        return ResponseEntity.status(HttpStatus.CREATED).body(updatedProductDTO);
+    }
+
+
+
+    @DeleteMapping("/{id}")
+    private void deleteProduct(@PathVariable String id, @RequestBody ProductDto productDto){
+
+    }
+
+
+    private void checkProductCategoryExistence(ProductDto productDto, String category_found_404) {
+        if (!productCategoryService.isExistedCategory(productDto)) {
+            throw new NoCategoryException(category_found_404);
+        }
+    }
 
 }
--- src/main/java/com/shopping/application/service/ProductService.java
+++ src/main/java/com/shopping/application/service/ProductService.java
@@ -2,20 +2,28 @@ package com.shopping.application.service;
 
 
 import com.shopping.application.dto.ProductDto;
+import com.shopping.application.exception.ProductNotFoundException;
+import com.shopping.application.exception.UuidConversionException;
 import com.shopping.application.mapper.ProductMapper;
 import com.shopping.application.models.Product;
 import com.shopping.application.repositorie.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class ProductService {
 
+
+
+
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+
     public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
         this.productRepository = productRepository;
         this.productMapper = productMapper;
@@ -23,20 +31,32 @@ public class ProductService {
 
     @Transactional
     public ProductDto createProduct(ProductDto productDto) {
-        Product product= productMapper.productDTOToProduct(productDto);
-        Product savedProduct= productRepository.save(product);
+        Product product = productMapper.productDTOToProduct(productDto);
+        Product savedProduct = productRepository.save(product);
         return productMapper.productToProductDTO(savedProduct);
     }
 
-    public ProductDto getProductById(String id) {
-        Product product= productRepository.findById(UUID.fromString(id)).orElse(null);
-        return productMapper.productToProductDTO(product);
-
+    @Transactional
+    public Optional<Product> getProductById(String id) {
+        try {
+            UUID uuid = UUID.fromString(id);
+        } catch (Exception e) {
+            throw new UuidConversionException();
+        }
+        Product product = productRepository.findById(UUID.fromString(id)).orElse(null);
+        return Optional.ofNullable(product);
     }
 
     public List<ProductDto> getAll() {
         List<Product> products = productRepository.findAll();
         List<ProductDto> productDtos = productMapper.mapProductsDToToProducts(products);
         return productDtos;
+    }
+
+    public Optional<Product> updateProduct(ProductDto productDto, String id) {
+        Product product = getProductById(id).orElseThrow(ProductNotFoundException::new);
+        Product mappedProduct = productMapper.productDTOToProduct(productDto);
+        Product updatedProduct = productRepository.save(product);
+        return Optional.of(updatedProduct);
     }
 }
