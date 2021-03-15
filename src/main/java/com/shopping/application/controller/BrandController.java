e415a96a496ef82dd4cde30bc773943979fa3bad
RAMI <ramiabdelilah1@gmail.com>
2021-03-15 13:59:20 +0100

    add @PostMapping

--- src/main/java/com/shopping/application/controller/BrandController.java
+++ src/main/java/com/shopping/application/controller/BrandController.java
@@ -6,6 +6,7 @@ import com.shopping.application.models.Brand;
 import com.shopping.application.repositorie.BrandRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class BrandController {
         this.brandRepository = brandRepository;
         this.brandMapper = brandMapper;
     }
-
+    @PostMapping
     public ResponseEntity<BrandDto> createBrand(@RequestBody BrandDto brandDto){
         Brand brand = brandMapper.brandDtoToBrand(brandDto);
         Brand savedBrand = brandRepository.save(brand);
